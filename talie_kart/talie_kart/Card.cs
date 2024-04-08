d6f28d42ea5def71e0e7cd527992cf7d12a0dd0a
Szymon-Rozwoda <rozwodaszymon2@gmail.com>
2024-04-08 12:01:19 +0200

    talia_kart

---
+++ talie_kart/talie_kart/Card.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace talie_kart
+{
+    
+        class Card
+        {
+            public Suits Suit { get; set; }
+            public Values Value { get; set; }
+
+            public Card(Suits suit, Values value)
+            {
+                this.Suit = suit;
+                this.Value = value;
+            }
+            public string Name
+            {
+                get
+                {
+                    return Value.ToString() + " of " + Suit.ToString();
+                }
+            }
+        }
+        enum Suits
+        {
+            Spades,
+            Clubs,
+            Diamonds,
+            Hearts
+        }
+        enum Values
+        {
+            Ace = 1,
+            Two = 2,
+            Three = 3,
+            Fourth = 4,
+            Five = 5,
+            Six = 6,
+            Seventh = 7,
+            Eventh = 8,
+            Nine = 9,
+            Ten = 10,
+            Jack = 11,
+            Queen = 12,
+            King = 13,
+
+        }
+    }
+
