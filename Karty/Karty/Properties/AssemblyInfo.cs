5392b3a961c9a76c62232404240192bb91d0ee54
Szymon-Rozwoda <rozwodaszymon2@gmail.com>
2024-04-08 09:54:50 +0200

    karty i lista

---
+++ Karty/Karty/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Karty")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Karty")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("ff36b07f-9989-4d8f-bd44-4375f6e1ff10")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lista_butow/Lista_butow/Program.cs
@@ -0,0 +1,54 @@
+﻿
+static void Main(string[] args)
+{
+    List<Shoe> shoeCloset = new List<Shoe>();
+
+    shoeCloset.Add(new Shoe()
+    {
+        Style = Style.Sneakers,
+        Color = "Czarny"
+    });
+    shoeCloset.Add(new Shoe()
+    {
+        Style = Style.Clogs,
+        Color = "Brązowy"
+    });
+    shoeCloset.Add(new Shoe()
+    {
+        Style = Style.Sandals,
+        Color = "Czerwony"
+    });
+
+    int numberOfShoes = shoeCloset.Count;
+    foreach(Shoe shoe in shoeCloset)
+    {
+        shoe.Style = Style.Flipflops;
+        shoe.Color = "Pomarańczowy";
+    }
+
+    shoeCloset.RemoveAt(4);
+
+    Shoe thirdShoe = shoeCloset[2];
+    Shoe secondShoe = shoeCloset[1];
+    shoeCloset.Clear();
+
+    shoeCloset.Add(thirdShoe);
+    if (shoeCloset.Contains(secondShoe))
+    {
+        Console.WriteLine("A to ci niespodzianka");
+    }
+}
+public class Shoe
+{
+    public Style Style;
+    public string Color;
+}
+public enum Style
+{
+    Sneakers,
+    Loafers,
+    Sandals,
+    Flipflops,
+    Wingtips,
+    Clogs
+}
