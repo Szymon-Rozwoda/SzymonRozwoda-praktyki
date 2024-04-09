6b6e303c49f111ec7a6f8423b695905594ef7cc7
Szymon Rozwoda <rozwodaszymon2@gmail.com>
2024-04-09 12:51:00 +0200

    stolowka_do_dopracowania

---
+++ stolowka_drwali/stolowka_drwali/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace stolowka_drwali
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.nextInLine = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.howMany = new System.Windows.Forms.NumericUpDown();
+            this.line = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.howMany)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Imię drwala";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Dodaj drwala";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(78, 9);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(159, 20);
+            this.name.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Kolejka do śniadania";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.nextInLine);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.howMany);
+            this.groupBox1.Location = new System.Drawing.Point(125, 97);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(112, 272);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nakarm drwala";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 91);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(79, 17);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Rumianego";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 243);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Następny drwal";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // nextInLine
+            // 
+            this.nextInLine.Location = new System.Drawing.Point(6, 166);
+            this.nextInLine.Multiline = true;
+            this.nextInLine.Name = "nextInLine";
+            this.nextInLine.ReadOnly = true;
+            this.nextInLine.Size = new System.Drawing.Size(85, 35);
+            this.nextInLine.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Dodaj naleśniki";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 114);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(88, 17);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Bananowego";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 68);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(79, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Wilgotnego";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 45);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(79, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Chrupkiego";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // howMany
+            // 
+            this.howMany.Location = new System.Drawing.Point(6, 19);
+            this.howMany.Name = "howMany";
+            this.howMany.Size = new System.Drawing.Size(67, 20);
+            this.howMany.TabIndex = 0;
+            // 
+            // line
+            // 
+            this.line.FormattingEnabled = true;
+            this.line.Location = new System.Drawing.Point(12, 110);
+            this.line.Name = "line";
+            this.line.Size = new System.Drawing.Size(105, 251);
+            this.line.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 383);
+            this.Controls.Add(this.line);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.howMany)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.NumericUpDown howMany;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox nextInLine;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox line;
+    }
+}
+
---
+++ stolowka_drwali/stolowka_drwali/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stolowka_drwali
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Queue<Lumberjack> breakfastLine = new Queue<Lumberjack>();
+        
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Flapjack food;
+            if(radioButton1.Checked == true)
+            {
+                food = Flapjack.Chrupkiego;
+            }
+            else if(radioButton2.Checked == true)
+            {
+                food = Flapjack.Wilgotnego;
+            }
+            else if(radioButton3.Checked == true)
+            {
+                food = Flapjack.Rumianego;
+            }
+            else
+            {
+                food = Flapjack.Banannowego;
+            }
+            Lumberjack currentLumberjack =breakfastLine.Peek();
+            currentLumberjack.TakeFlapjacks(food, (int)howMany.Value);
+            RedrawList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(name.Text)) return;
+            breakfastLine.Enqueue(new Lumberjack(name.Text));
+            name.Text = "";
+            RedrawList();
+        }
+        private void RedrawList()
+        {
+            int number = 1;
+            line.Items.Clear();
+            foreach(Lumberjack lumberjack in breakfastLine)
+            {
+                line.Items.Add(number+". "+lumberjack.Name);
+            }
+        }
+    }
+    //lumberjacks
+
+    public class Lumberjack
+    {
+        private string name;
+        public string Name { get { return name; } }
+        private Stack<Flapjack> meal;
+        public Lumberjack(string name)
+        {
+            this.name = name;
+            meal = new Stack<Flapjack>();
+        }
+        public int FlapjackCount { get { return meal.Count; } }
+        public void TakeFlapjacks(Flapjack food, int HowMany)
+        {
+            for (int i = 0; i < HowMany; i++)
+            {
+                meal.Push(food);
+            }
+        }
+        public void EatFlapjacks()
+        {
+            Console.WriteLine(name + " je naleśniki");
+            while (meal.Count > 0)
+            {
+                Console.WriteLine(name+" zjadł "+meal.Pop().ToString().ToLower()+" naleśnika");
+            }
+        }
+
+    }
+    public enum Flapjack
+    {
+        Chrupkiego,
+        Wilgotnego,
+        Rumianego,
+        Banannowego
+    }
+}
