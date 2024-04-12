c6fb1b136dc3a6089807d77ad8edc26713a71a72
Szymon Rozwoda <rozwodaszymon2@gmail.com>
2024-04-12 09:45:24 +0200

    gra_karciana_ryby

---
+++ gra_karciana_ryby2/gra_karciana_ryby2/Suits.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gra_karciana_ryby2
+{
+    enum Suits
+    {
+        Spades,
+        Clubs,
+        Diamonds,
+        Hearts
+    }
+}
