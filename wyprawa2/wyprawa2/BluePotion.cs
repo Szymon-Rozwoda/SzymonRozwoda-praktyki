86b1a2b2aa0662b2431944d778ccac0b5f4b92c7
Szymon Rozwoda <rozwodaszymon2@gmail.com>
2024-04-15 10:38:01 +0200

    wyprawa_gra_nowa_dzialajaca_wersja

---
+++ wyprawa2/wyprawa2/BluePotion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wyprawa2
+{
+    public class BluePotion : Weapon, IPotion
+    {
+        public BluePotion(Game game, Point location)
+            : base(game, location) { Used = false; }
+
+        public bool Used { get; private set; }
+
+        public override string Name { get { return "Blue Potion"; } }
+        public override void Attack(Direction direction, Random random)
+        {
+            game.IncreasePlayerHealth(5, random);
+            Used = true;
+        }
+    }
+}
---
+++ wyprawa2/wyprawa2/ISprite.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wyprawa2
+{
+    interface ISprite
+    {
+        Size SpriteSize { get; }
+    }
+}
