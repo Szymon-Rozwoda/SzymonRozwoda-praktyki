25ba09b11ddfb7a0672c5faa11b6eb7c95caa802
Szymon Rozwoda <rozwodaszymon2@gmail.com>
2024-04-15 07:59:50 +0200

    disposeVsFinalizator

---
+++ disposeVSfinalizator/disposeVSfinalizator/Clone.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace disposeVSfinalizator
+{
+    [Serializable]
+    class Clone :IDisposable
+    {
+        
+        public int Id { get;private set; }
+
+        public Clone(int Id)
+        {
+            this.Id = Id;
+        }
+        public void Dispose()
+        {
+            string filename = @"C:\Temp\Klon.dat";
+            string dirname = @"C:\Temp\";
+            if (File.Exists(filename) == false)
+            {
+                Directory.CreateDirectory(dirname);
+            }
+            BinaryFormatter bf = new BinaryFormatter();
+            using (Stream output = File.OpenWrite(filename))
+            {
+                bf.Serialize(output, this);
+            }
+            MessageBox.Show("Tu " + this.Id + ", muszę... zserializować... obiekt.");
+            //MessageBox.Show("Zostałem usuniety!", "Klon" + Id + ". mówi...");
+        }
+        Clone()
+        {
+            MessageBox.Show("Aaaaaa! Dopadłeś mnie!", "Klon " + Id + ". mówi...");
+        }
+    }
+}
