200698715bfeb4f20b35ab4a84f25ace6fa25877
Szymon Rozwoda <rozwodaszymon2@gmail.com>
2024-04-15 09:15:33 +0200

    dzienNaWyscigach_DZIALA

---
+++ Dzien_na_wyscigach2/Dzien_na_wyscigach2/Greyhound.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dzien_na_wyscigach2
+{
+    public class Greyhound
+    {
+        public int StartingPosition;
+        public int RacetrackLength;
+        public PictureBox MyPictureBox = null;
+        public int Location = 0;
+        public Random MyRandom;
+
+        public bool Run()
+        {
+            MyRandom = new Random();
+            int distance = MyRandom.Next(1, 5);
+
+            MoveMyPictureBox(distance);
+
+            Location += distance;
+            if (Location >= (RacetrackLength - StartingPosition))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void TakeStartingPosition()
+        {
+            MoveMyPictureBox(-Location);
+            Location = 0;
+        }
+
+        public void MoveMyPictureBox(int distance)
+        {
+            Point p = MyPictureBox.Location;
+            p.X += distance;
+            MyPictureBox.Location = p;
+        }
+    }
+}
